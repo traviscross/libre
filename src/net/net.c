ddc3caac2d1ee695671bf4634f128b61caebd06c
Travis Cross <tc@traviscross.com>
2015-07-01 21:11:29 +0000

    Import re-0.4.13.tar.gz
    source: http://creytiv.com/pub/re-0.4.13.tar.gz

--- src/net/net.c
+++ src/net/net.c
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2010 Creytiv.com
  */
+#define _BSD_SOURCE 1
 #include <stdlib.h>
 #include <string.h>
 #if !defined(WIN32) && !defined(CYGWIN)
@@ -103,4 +104,53 @@ int net_if_apply(net_ifaddr_h *ifh, void *arg)
 #else
 	return net_if_list(ifh, arg);
 #endif
+}
+
+
+static bool net_rt_handler(const char *ifname, const struct sa *dst,
+			   int dstlen, const struct sa *gw, void *arg)
+{
+	void **argv = arg;
+	struct sa *ip = argv[1];
+	(void)dst;
+	(void)dstlen;
+
+	if (0 == str_cmp(ifname, argv[0])) {
+		*ip = *gw;
+		return true;
+	}
+
+	return false;
+}
+
+
+/**
+ * Get the IP-address of the default gateway
+ *
+ * @param af  Address Family
+ * @param gw  Returned Gateway address
+ *
+ * @return 0 if success, otherwise errorcode
+ */
+int net_default_gateway_get(int af, struct sa *gw)
+{
+	char ifname[64];
+	void *argv[2];
+	int err;
+
+	if (!af || !gw)
+		return EINVAL;
+
+	err = net_rt_default_get(af, ifname, sizeof(ifname));
+	if (err)
+		return err;
+
+	argv[0] = ifname;
+	argv[1] = gw;
+
+	err = net_rt_list(net_rt_handler, argv);
+	if (err)
+		return err;
+
+	return 0;
 }
